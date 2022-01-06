0cd1b81f3d21e99d06fadb6ea32e7cf11f3defa3
Anthony Salemo <ajssalemo@gmail.com>
2022-01-06 18:29:20 -0500

    integrated TailwindCSS w/Yarn into the project

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,6 +10,8 @@
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
         <!-- normalize.css v8.0.1 | MIT License | github.com/necolas/normalize.css  -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <!-- TailwindCSS output -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
     <body>
         @yield('content')
