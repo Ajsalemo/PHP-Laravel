e7109b6d7fe2c4a35937e39e7d353888a0c2a6ef
Anthony Salemo <ajssalemo@gmail.com>
2022-01-09 11:03:38 -0500

    added partials to the template and a navbar

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,19 +1,24 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        <!-- normalize.css v8.0.1 | MIT License | github.com/necolas/normalize.css  -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-        <!-- TailwindCSS output -->
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    </head>
-    <body>
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <!-- normalize.css v8.0.1 | MIT License | github.com/necolas/normalize.css  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- TailwindCSS output -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    @include('partials.header')
+    <div class="container mx-auto px-4">
         @yield('content')
-    </body>
+    </div>
+</body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,12 @@
 @extends("layouts.main")
 
 @section('content')
-<h1 class="text-3xl font-bold underline">
-    Hello world!
-</h1>
+    <h1 class="text-3xl font-bold underline">
+        PHP Laravel
+    </h1>
+    <ul>
+    @for($i = 0; $i < 4; $i++)
+        <li>{{ $i }}</li>
+    @endfor
+    </ul>
 @endsection
