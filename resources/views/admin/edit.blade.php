92e6f56a0ac1c86a462a04e2723807afd439e898
Anthony Salemo <ajssalemo@gmail.com>
2022-01-20 17:12:15 -0500

    put the session php block into its own partial

--- resources/views/admin/edit.blade.php
+++ resources/views/admin/edit.blade.php
@@ -19,18 +19,7 @@
             </div>
         </div>
         <div class="mt-5 md:mt-0 md:col-span-2">
-            <!-- If the session key exists, display the information -->
-            @if(Session::get('success'))
-            <div class="bg-green-300 border border-green-300 px-4 py-3 relative shadow overflow-hidden sm:rounded-md" role="alert">
-                <span class="block sm:inline">{{ Session::get('success') }}</span>
-                <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
-                    <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                        <title>Close</title>
-                        <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
-                    </svg>
-                </span>
-            </div>
-            @endif
+            @include('partials.session')
             <form action="{{ route('admin.editsubmit' )}}" method="POST">
                 <div class="shadow overflow-hidden sm:rounded-md">
                     <div class="px-4 py-5 bg-white sm:p-6">
--- routes/web.php
+++ routes/web.php
@@ -14,90 +14,90 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::get('/', function () {
-    return view('blog.index');
-})->name('blog.index');
+Route::get("/", function () {
+    return view("blog.index");
+})->name("blog.index");
 
-Route::group(['prefix' => 'blog'], function () {
-    Route::get('post/view/{id}', function ($id) {
+Route::group(["prefix" => "blog"], function () {
+    Route::get("post/view/{id}", function ($id) {
         // This is mock data for the time being
         if ($id == 1) {
             $postDetail = [
-                'id' => 1,
-                'title' => 'This is a fake title',
-                'firstname' => 'Some',
-                'lastname' => 'User',
-                'content' => 'Lorem Ipsum is simply dummy text of the printing and
+                "id" => 1,
+                "title" => "This is a fake title",
+                "firstname" => "Some",
+                "lastname" => "User",
+                "content" => "Lorem Ipsum is simply dummy text of the printing and
                 typesetting industry. Lorem Ipsum is simply dummy text of the printing and
                 typesetting industry. Lorem Ipsum is simply dummy text of the printing and
                 typesetting industry. Lorem Ipsum is simply dummy text of the printing and
-                typesetting industry.'
+                typesetting industry."
             ];
-            return view('blog.view', ['post' => $postDetail]);
+            return view("blog.view", ["post" => $postDetail]);
         } else {
-            return view('blog.view');
+            return view("blog.view");
         }
-    })->name('blog.view');
+    })->name("blog.view");
 
-    Route::get('post/get', function () {
+    Route::get("post/get", function () {
         // This is mock data for the time being
         $post = [
-            'id' => 1,
-            'title' => 'This is a fake title',
-            'firstname' => 'Some',
-            'lastname' => 'User',
-            'content' => 'Lorem Ipsum is simply dummy text of the printing and
+            "id" => 1,
+            "title" => "This is a fake title",
+            "firstname" => "Some",
+            "lastname" => "User",
+            "content" => "Lorem Ipsum is simply dummy text of the printing and
             typesetting industry. Lorem Ipsum is simply dummy text of the printing and
             typesetting industry. Lorem Ipsum is simply dummy text of the printing and
             typesetting industry. Lorem Ipsum is simply dummy text of the printing and
-            typesetting industry.'
+            typesetting industry."
         ];
-        return view('blog.posts', ['post' => $post]);
-    })->name('blog.posts');
+        return view("blog.posts", ["post" => $post]);
+    })->name("blog.posts");
 });
 
 
-Route::group(['prefix' => 'admin'], function () {
-    Route::get('', function () {
+Route::group(["prefix" => "admin"], function () {
+    Route::get("", function () {
         $adminPost = [
-            'id' => 1,
-            'title' => 'This is a fake title',
-            'firstname' => 'Some',
-            'lastname' => 'User',
-            'content' => 'Lorem Ipsum is simply dummy text of the printing and
+            "id" => 1,
+            "title" => "This is a fake title",
+            "firstname" => "Some",
+            "lastname" => "User",
+            "content" => "Lorem Ipsum is simply dummy text of the printing and
             typesetting industry. Lorem Ipsum is simply dummy text of the printing and
             typesetting industry. Lorem Ipsum is simply dummy text of the printing and
             typesetting industry. Lorem Ipsum is simply dummy text of the printing and
-            typesetting industry.'
+            typesetting industry."
         ];
 
-        return view('admin.index', ['post' => $adminPost]);
-    })->name('admin.index');
+        return view("admin.index", ["post" => $adminPost]);
+    })->name("admin.index");
 
-    Route::get('post/new', function () {
-        return view('admin.newpost');
-    })->name('admin.newpost');
+    Route::get("post/new", function () {
+        return view("admin.newpost");
+    })->name("admin.newpost");
 
-    Route::post('post/new/submit', function () {
-        return redirect('admin')->with('success', 'Post successful.');
-    })->name('admin.create');
+    Route::post("post/new/submit", function () {
+        return redirect("admin")->with("success", "Post successful.");
+    })->name("admin.create");
 
-    Route::post('post/edit/submit', function () {
-        return redirect()->back()->with('success', 'Edit successful.');
-    })->name('admin.editsubmit');
+    Route::post("post/edit/submit", function () {
+        return redirect()->back()->with("success", "Edit successful.");
+    })->name("admin.editsubmit");
 
-    Route::get('post/edit/{id}', function () {
+    Route::get("post/edit/{id}", function () {
         $adminEditPost = [
-            'id' => 1,
-            'title' => 'This is a fake title',
-            'firstname' => 'Some',
-            'lastname' => 'User',
-            'content' => 'Lorem Ipsum is simply dummy text of the printing and
+            "id" => 1,
+            "title" => "This is a fake title",
+            "firstname" => "Some",
+            "lastname" => "User",
+            "content" => "Lorem Ipsum is simply dummy text of the printing and
             typesetting industry. Lorem Ipsum is simply dummy text of the printing and
             typesetting industry. Lorem Ipsum is simply dummy text of the printing and
             typesetting industry. Lorem Ipsum is simply dummy text of the printing and
-            typesetting industry.'
+            typesetting industry."
         ];
-        return view('admin.edit', ['adminEditPost' => $adminEditPost]);
-    })->name('admin.edit');
+        return view("admin.edit", ["adminEditPost" => $adminEditPost]);
+    })->name("admin.edit");
 });
