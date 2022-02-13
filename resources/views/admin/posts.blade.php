d209e76b84ba1c20ce2e72e41e2dd94b86a94805
Anthony Salemo <ajssalemo@gmail.com>
2022-02-12 19:36:47 -0500

    changing to show only view post button on bulk post route

--- resources/views/admin/posts.blade.php
+++ resources/views/admin/posts.blade.php
@@ -52,7 +52,9 @@
             Edit Post
         </a>
     </div>
-    @else
+    @endif
+    
+    @if(Request::is('blog/post/get'))
     <div class="pt-4">
         <a href="{{ route('blog.view', ['id' => $post->id] )}}" class="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
             View Post
