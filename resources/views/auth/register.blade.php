1ff4d3a1e1f8eb32c9080eae9ec52ce3e1351b03
Anthony Salemo <ajssalemo@gmail.com>
2022-02-18 15:24:29 -0500

    updating the register template

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -15,8 +15,8 @@
             <input type="hidden" name="remember" value="true">
             <div class="rounded-md shadow-sm -space-y-px">
                 <div>
-                    <label for="name" class="text-sm">Name</label>
-                    <input id="name" name="name" type="name" autocomplete="name"value="{{ old('name') }}" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Name">
+                    <label for="name" class="text-sm">{{ __('Name') }}</label>
+                    <input id="name" name="name" type="name" autocomplete="name" value="{{ old('name') }}" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Name">
                     @error('name')
                     <span class="text-red-300" role="alert">
                         <strong>{{ $message }}</strong>
@@ -24,8 +24,8 @@
                     @enderror
                 </div>
                 <div>
-                    <label for="email" class="text-sm">Email</label>
-                    <input id="email" email="email" type="email" autocomplete="email"value="{{ old('email') }}" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="name@email.com">
+                    <label for="email" class="text-sm">{{ __('Email') }}</label>
+                    <input id="email" email="email" type="email" autocomplete="email" value="{{ old('email') }}" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="name@email.com">
                     @error('email')
                     <span class="text-red-300" role="alert">
                         <strong>{{ $message }}</strong>
@@ -33,7 +33,7 @@
                     @enderror
                 </div>
                 <div>
-                    <label for="password" class="text-sm">Password</label>
+                    <label for="password" class="text-sm">{{ __('Password') }}</label>
                     <input id="password" name="password" type="password" autocomplete="current-password" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password">
                     @error('password')
                     <span class="text-red-300" role="alert">
@@ -42,8 +42,8 @@
                     @enderror
                 </div>
                 <div>
-                    <label for="password-confirm" class="text-sm">Confirm Password</label>
-                    <input id="password-confirm" name="password-confirm" type="password-confirm" autocomplete="new-password" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Confirm Password">
+                    <label for="password-confirm" class="text-sm">{{ __('Confirm Password') }}</label>
+                    <input id="password-confirm" name="password_cofirmation" type="password" autocomplete="new-password" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Confirm Password">
                     @error('password-confirm')
                     <span class="text-red-300" role="alert">
                         <strong>{{ $message }}</strong>
@@ -52,6 +52,7 @@
                 </div>
             </div>
             <div>
+                {{ csrf_field() }}
                 <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                     <span class="absolute left-0 inset-y-0 flex items-center pl-3">
                         <!-- Heroicon name: solid/lock-closed -->
@@ -59,7 +60,7 @@
                             <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
                         </svg>
                     </span>
-                    Sign in
+                    {{ __('Register') }}
                 </button>
             </div>
         </form>
